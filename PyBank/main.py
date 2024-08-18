85a6ec82303cb482d6d2262c6309c0a6a5e70e96
Feda Zidan <fido311@gmail.com>
2024-08-18 13:12:10 -0500

    Moved the main files back to the parent folder

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,27 +4,27 @@ import os
 #Module for reading CSV files
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'election_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
 # Method 2: Improved Reading using CSV module
 
-with open(csvpath) as csvfile:
+with open(csvpath , 'r') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    
-#To skip the header row
+
+    #To skip the header row
     next(csvreader)
 
     #Count the totaly number of months 
     
-    total_votes = sum(1 for row in csvreader)
+    total_months = sum(1 for row in csvreader)
     
     #printing the data title then printing a line
-    print("Election Results")
+    print("Finanacial Analysis")
 
     print("--------------------------------")
     #print the total number of months
-    print(f"Total number of months: {total_votes}")
+    print(f"Total number of months: {total_months}")
 
-    print("--------------------------------")
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,27 +4,27 @@ import os
 #Module for reading CSV files
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'election_data.csv')
 
 # Method 2: Improved Reading using CSV module
 
-with open(csvpath , 'r') as csvfile:
+with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    #To skip the header row
+    
+#To skip the header row
     next(csvreader)
 
     #Count the totaly number of months 
     
-    total_months = sum(1 for row in csvreader)
+    total_votes = sum(1 for row in csvreader)
     
     #printing the data title then printing a line
-    print("Finanacial Analysis")
+    print("Election Results")
 
     print("--------------------------------")
     #print the total number of months
-    print(f"Total number of months: {total_months}")
+    print(f"Total number of months: {total_votes}")
 
-
+    print("--------------------------------")
